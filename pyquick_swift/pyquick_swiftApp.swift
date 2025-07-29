320cce341ea98fe668f14807d59c6cd520bcd216
Code Jumping <fairzlq@hotmail.com>
2025-07-29 19:48:31 +0800

    Initial Commit

---
+++ pyquick_swift/pyquick_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  pyquick_swiftApp.swift
+//  pyquick_swift
+//
+//  Created by Rak on 7/29/25.
+//
+
+import SwiftUI
+
+@main
+struct pyquick_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
